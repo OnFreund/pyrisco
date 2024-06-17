4922ef29a31fba30c95c56aea4d2b2d19fd18dfc
On Freund <onfreund@gmail.com>
2024-06-17 20:30:12 +0300

    0.6.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/OnFreund/PyRisco'
 EMAIL = 'onfreund@gmail.com'
 AUTHOR = 'On Freund'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.6.2'
+VERSION = '0.6.3'
 
 REQUIRED = ['aiohttp']
 
