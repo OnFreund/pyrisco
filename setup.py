cfd362ea80909e304f562de74c79e78d544f046c
jakob1911 <112578977+jakob1911@users.noreply.github.com>
2024-06-24 21:19:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/OnFreund/PyRisco'
 EMAIL = 'onfreund@gmail.com'
 AUTHOR = 'On Freund'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.6.4'
+VERSION = '0.66.5'
 
 REQUIRED = ['aiohttp']
 
