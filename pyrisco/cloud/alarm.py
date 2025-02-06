518d9942984794a4aea5a0e8d3a231fb7e057c53
Alex <alexandre.guillon@mckay-brothers.com>
2025-02-06 17:39:29 +0100

    RiscoCloud: Add assumed_control_panel_state and automatic fallback to cloud (#31)
    * RiscoCloud: Add assumed_control_panel_state and automatic fallback to cloud
    
    restore from_control_panel=True from version 0.6.4
    add RetryableOperationError when code 72 is returned from RiscoCloud
    switch to from_control_panel=False when RetryableOperationError is raised once
    set Alarm.is_from_control_panel flag according to _site_post's from_control_panel
    
    bump version to 0.6.6

--- pyrisco/cloud/alarm.py
+++ pyrisco/cloud/alarm.py
@@ -4,12 +4,18 @@ from .zone import Zone
 class Alarm:
   """A representation of a Risco alarm system."""
 
-  def __init__(self, api, raw):
+  def __init__(self, api, raw, assumed_control_panel_state):
     """Read alarm from response."""
     self._api = api
     self._raw = raw
     self._partitions = None
     self._zones = None
+    self._assumed_control_panel_state = assumed_control_panel_state
+
+  @property
+  def assumed_control_panel_state(self):
+    """Return True if the state is based on RiscoCloud instead of reading it from the control panel."""
+    return self._assumed_control_panel_state
 
   @property
   def partitions(self):
