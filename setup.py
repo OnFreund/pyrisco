5609936b28966503da8faca8e32cfe14cf00fc3d
On Freund <onfreund@gmail.com>
2026-04-10 19:09:45 +0300

    Bump version from 0.6.7 to 0.6.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/OnFreund/PyRisco'
 EMAIL = 'onfreund@gmail.com'
 AUTHOR = 'On Freund'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.6.7'
+VERSION = '0.6.8'
 
 REQUIRED = ['aiohttp']
 
